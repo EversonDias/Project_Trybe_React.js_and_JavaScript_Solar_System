2e2305c3b1ba155447e06a7cdc109895cc2a00e6
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-25 16:24:47 -0300

    create component PlanetCard

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <img src={ planetImage } alt={`Planeta ${planetName}`} />
+        <p data-testid="planet-name">{ planetName }</p>
+      </div>
+    )
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropType.string.isRequired,
+  planetImage: PropType.string.isRequired,
+}
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    const headline = this.props
+    const { headline } = this.props;
     return (
       <h2>{headline}</h2>
-    )
+    );
   }
 }
+
+Title.propTypes = {
+  headline: PropType.string.isRequired,
+};
 
 export default Title;
